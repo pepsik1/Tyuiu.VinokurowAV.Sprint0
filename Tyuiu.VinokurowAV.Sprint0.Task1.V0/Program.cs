a8d1f36faf78ecb324c1db9c22b9a556bcab08d6
pepsik1 <artemvinokurow78@gmail.com>
2026-09-10 15:11:27 +0500

    Task1

---
+++ Tyuiu.VinokurowAV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VinokurowAV.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Артём";
+            int age = 18;
+
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+
+            Console.ReadKey();
+        }
+    }
+}
